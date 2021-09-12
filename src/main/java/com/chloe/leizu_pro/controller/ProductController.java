78344a1a9b03e11543c490a40bf60bc118d2ed1f
chloe <chloe@gmail.com>
2021-09-12 20:38:42 +0800

    09122030_mainPage_添加自動添加數據功能

--- src/main/java/com/chloe/leizu_pro/controller/ProductController.java
+++ src/main/java/com/chloe/leizu_pro/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.chloe.leizu_pro.controller;
 
 
 import com.chloe.leizu_pro.bean.Product;
-import com.chloe.leizu_pro.bean.SuperCategory;
 import com.chloe.leizu_pro.service.ProductService;
 import com.chloe.leizu_pro.utils.ProductPageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class ProductController {
@@ -64,6 +60,14 @@ public class ProductController {
         ModelAndView mav = new ModelAndView();
         ProductPageUtils.productDetailsPage(mav, productService, "women", productId, colorName);
         return mav;
+    }
+
+    @RequestMapping(value = "/load/{gender}", method = RequestMethod.POST)
+    @ResponseBody
+    public List<Product> loadMoreProduct(@PathVariable("gender") String gender){
+        int  indexRan = (int)(Math.random()*50+1);
+        List<Product> productListByLimit = productService.getProductListByLimit( indexRan, 20, gender);
+        return productListByLimit;
     }
 
 
---
+++ src/main/resources/static/js/product_main.js
@@ -0,0 +1,39 @@
+
+
+$(function (){
+    var height = $("html body").innerHeight() - 450;
+    var $dest =  $(".product_section > ul");
+
+    $(window).stop().scroll(function (){
+        if (height - $(this).scrollTop() < 0){
+            $.ajax({
+                url: "/load/" + gender,
+                type: "post",
+                success: function (data){
+                    for (const index in data) {
+                        var product = data[index];
+                        var $li = $('<li></li>');
+                        var url = `/${gender}/details/${product.id}`;
+                        var $a = $('<a href="'+ url+'"></a>');
+                        var $img = $('<img src="'+ product["image"]+'" alt="'+ product["productName"] +'" title="'+ product["productName"] +'">');
+                        var $h3 = $('<h3>'+ product["productName"]  +'</h3>');
+                        var $p;
+                        if (product["promo"] == null){
+                            $p = $('<p>NT$ '+ product["price"] +'</p>');
+                        } else {
+                            $p = $('<p><del>NT$'+ product["price"] +'</del><span>活動價NT$ '+ product["promoPrice"] +'</span></p>');
+                        }
+                        $a.append($img);
+                        $li.append($a).append($h3).append($p);
+                        $dest.append($li);
+                    }
+                    height = $("html body").innerHeight() - 450;
+                },
+                error: function (error){
+                    console.log(error);
+                }
+
+            });
+        }
+    });
+});

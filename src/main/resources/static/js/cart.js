3f066a1994da655061156dbe5b8f5f269fcca40b
chloe <chloe@gmail.com>
2021-09-14 17:12:34 +0800

    09141700_ 購物車頁面，差 select改尺寸，購買數量限制（商品庫存），刪除購物車

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,28 +1,65 @@
-var colorIdAndName;
-var sizeId;
-var quantity;
-var product_cart;
-
-$(function(){
-    $("#addCart").click(function (){
-        colorIdAndName = $(".color").find(".current").attr("value");
-        sizeId = $(".size").find(".current").attr("value");
-        quantity = $("#buyNumber").val();
-
-        product_cart = {
-            id: colorIdAndName.substr(0,7) + sizeId,
-            buyNumber: quantity
-        };
-
-        $.ajax({
-            url: "/shop/cart",
-            type: "post",
-            data: {product:JSON.stringify(product_cart)},
-            success: function (){
-            },
-            error: function (error){
-                console.log(error);
-            }
-        });
+$(function (){
+    $("#addAll").click(function (){
+        var $boxArr = $("tbody").find("input:checkbox").prop("checked", $(this).prop("checked"));
+        countPrice(true, $(this));
     });
+
+    $("tbody").find("input:checkbox").click(function (){
+        if ($("#addAll").prop("checked") == true && $(this).prop("checked") == false){
+            $("#addAll").prop("checked", false);
+        }
+        countPrice(false, $(this).parents("tr"));
+    });
+
+    function countPrice(all, $item){
+        var originalTotalPrice = 0;
+        var discount = 0;
+        var totalPrice = 0;
+
+        if(all) {
+            if ($item.prop("checked")) {
+                var $h5Arr = $("tbody").find("h5");
+                var $numArr = $("tbody").find(".buyNumber");
+                for (var i = 0; i < $h5Arr.length; i++) {
+                    var priceArr = $h5Arr.eq(i).attr("value").split(",");
+                    var number = $numArr.eq(i).val();
+
+                    originalTotalPrice += priceArr[0] * number;
+
+                    if (!isNaN(parseInt(priceArr[1]))) {
+                        // 有優惠情形下
+                        discount += -(priceArr[0] - priceArr[1]) * number;
+                    }
+                    totalPrice = originalTotalPrice + discount + 60;
+                }
+            }
+        } else {
+            originalTotalPrice = parseInt($("#product_price").text().substring(3).trim());
+            discount = parseInt($("#discount_price").text().substring(3).trim());
+            totalPrice = parseInt($("#total_price").text().substring(3).trim());
+            var priceArr = $item.find("h5").attr("value").split(",");
+            var number = $item.find(".buyNumber").val();
+
+            if ($item.find("input:checkbox").prop("checked")){
+                originalTotalPrice += parseInt(priceArr[0])*number;
+                if (!isNaN(priceArr[1])){
+                    discount -= (priceArr[0] - priceArr[1])*number;
+                }
+            } else {
+                originalTotalPrice -= parseInt(priceArr[0])*number;
+                if (!isNaN(priceArr[1])){
+                    discount += (priceArr[0] - priceArr[1])*number;
+                }
+            }
+            totalPrice = originalTotalPrice + discount + 60;
+
+        }
+
+            $("#product_price").text('NT$ '+ originalTotalPrice);
+            $("#discount_price").text('NT$ ' + discount);
+            $("#total_price").text('NT$ ' + totalPrice);
+        }
+
+
+
 });
